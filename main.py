6c9a0089bbd69550f48740a6ee8dbe9e444616e0
Ryan OConnor <r2oconnor@student.bridgew.edu>
2021-10-07 18:49:34 -0400

    match line with randomly generated question #

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ def rand_num_gen():
 
 
 if __name__ == '__main__':
-    aws_test_file = "aws_test.txt"
 
-    while True:
-        question_from_text = rand_num_gen()
-        print(question_from_text)
+    keep_going = True
 
+    while keep_going:
+        question_from_text = "QUESTION " + str(rand_num_gen())
+        with open("aws_test.txt", "r") as aws_test_file:
+            searchlines = aws_test_file.readlines()
+        for i, line in enumerate(searchlines):
+            if str(question_from_text) == line.rstrip():
+                print("    LINE #: " + str(i+1))
+                print("LINE VALUE: " + line)
+            keep_going = False
 
-
+        #print(question_from_text)
