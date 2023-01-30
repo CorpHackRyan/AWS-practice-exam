08eaa32c18eed1b98f62a96526c24c1c622452fc
Kaushik Chemburkar <attherate@gmail.com>
2023-01-30 15:59:12 +1100

    Keeping a score and Do you want to continue ?

--- main.py
+++ main.py
@@ -5,25 +5,31 @@ def rand_num_gen():
     return question_num
 
 if __name__ == '__main__':
+    total_questions = 0
+    correct_answers = 0
 
     keep_going = True
-
     while keep_going:
         question_from_text = "QUESTION " + str(rand_num_gen())
         with open("aws_test.txt", "r") as aws_test_file:
             searchlines = aws_test_file.readlines()
         for i, line in enumerate(searchlines):
-            if str(question_from_text) == line.rstrip():
+            if str(question_from_text) in line:
                 question_index = i
-                question_num = line.split(' ')[1]
-                print("LINE NUMBER: " + str(question_index + 1))
-                print("QUESTION NUMBER: " + question_num)
-                print("QUESTION: " + searchlines[question_index + 1].rstrip())
-                print("OPTIONS:")
-                option_index = question_index + 2
-                while "Correct Answer" not in searchlines[option_index]:
-                    print(searchlines[option_index].rstrip())
-                    option_index += 1
-                correct_answer = searchlines[option_index].split(': ')[1].rstrip()
-                print("\nCorrect Answer: " + correct_answer)
-                keep_going = False
+                print(line)
+                for j in range(question_index+1, len(searchlines)):
+                    if "Correct Answer" in searchlines[j]:
+                        correct_answer = searchlines[j].split(":")[1].strip()
+                        break
+                    print(searchlines[j].strip())
+                total_questions += 1
+                user_answer = input("Enter your answer: ")
+                if user_answer.strip() == correct_answer:
+                    correct_answers += 1
+                    print("Correct!")
+                else:
+                    print(f"Incorrect! The correct answer is {correct_answer}")
+                break
+        score = correct_answers/total_questions * 100
+        print(f"Score: {correct_answers}/{total_questions} ({score:.2f}%)")
+        keep_going = input("Do you want to continue? (y/n) ") == 'y'
