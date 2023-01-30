f9b58056775c52e0144ed5008493eaa7b1dc3c09
Kaushik Chemburkar <attherate@gmail.com>
2023-01-30 18:43:42 +1100

    Terminal looks more organised

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         for i, line in enumerate(searchlines):
             if str(question_from_text) in line:
                 question_index = i
-                print(line)
+                print("\n" + line)
                 for j in range(question_index+1, len(searchlines)):
                     if "Correct Answer" in searchlines[j]:
                         correct_answer = searchlines[j].split(":")[1].strip()
@@ -28,7 +28,7 @@ if __name__ == '__main__':
                 question_start_time = time.time()
                 user_answer = input("Enter your answer: ")
                 question_elapsed_time = time.time() - question_start_time
-                print(f"Time taken for this question: {question_elapsed_time//60:.0f} minutes and {question_elapsed_time%60:.0f} seconds")
+
                 if user_answer.strip() == correct_answer:
                     correct_answers += 1
                     print("Correct!")
@@ -38,5 +38,6 @@ if __name__ == '__main__':
         score = correct_answers/total_questions * 100
         elapsed_time = time.time() - start_time
         print(f"Score: {correct_answers}/{total_questions} ({score:.2f}%)")
+        print(f"Time taken for this question: {question_elapsed_time // 60:.0f} minutes and {question_elapsed_time % 60:.0f} seconds")
         print(f"Total time taken: {elapsed_time//3600:.0f} hours and {(elapsed_time%3600)//60:.0f} minutes")
-        keep_going = input("Do you want to continue? (y/n) ") == 'y'
+        keep_going = input("\n" + "Do you want to continue? (y/n) ") == 'y'
